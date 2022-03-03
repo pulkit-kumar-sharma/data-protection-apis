eb3a4d9900c2a78ac1b99e01178efff717757ff7
Sharma, Pulkit K <pulkit.sharma@optum.com>
2022-03-04 02:43:02 +0530

    Update TokenizationController.java

--- src/main/java/com/pulkit/dataprotection/controller/TokenizationController.java
+++ src/main/java/com/pulkit/dataprotection/controller/TokenizationController.java
@@ -2,13 +2,11 @@ package com.pulkit.dataprotection.controller;
 
 import com.pulkit.dataprotection.service.TokenizationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/")
+@CrossOrigin("*")
 public class TokenizationController {
 
     @Autowired
