b31424c0137bf47002a6567d580bb09dd647c394
Sharma, Pulkit K <pulkit.sharma@optum.com>
2022-03-03 23:33:22 +0530

    Added Request Mapping

--- src/main/java/com/pulkit/dataprotection/controller/TokenizationController.java
+++ src/main/java/com/pulkit/dataprotection/controller/TokenizationController.java
@@ -4,9 +4,11 @@ import com.pulkit.dataprotection.service.TokenizationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/")
 public class TokenizationController {
 
     @Autowired
